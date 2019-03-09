c17787d50b07144101109430fabe79af0b36bbff
Bogdan Tanase <bftanase@gmail.com>
2019-03-09 17:12:58 +0200

    Add project files.

---
+++ bft-crypto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bft_crypto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
